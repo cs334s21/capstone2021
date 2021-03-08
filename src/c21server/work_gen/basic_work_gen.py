13c7746df852eebbed9720f51a77954671a01900
Colby Hillman <colby.hillman@gmail.com>
2021-03-08 17:19:24 -0500

    Cleaned up code style of basic_work_gen.py

--- src/c21server/work_gen/basic_work_gen.py
+++ src/c21server/work_gen/basic_work_gen.py
@@ -1,22 +1,22 @@
-import redis
 import random
 import time
+import redis
 
-r = redis.Redis()
+redis_database = redis.Redis()
 
 
-def generateJobs(r):
-    for i in range(10):
-        id = random.randint(0, 10000)
+def generate_jobs(database):
+    for _ in range(10):
+        client_id = random.randint(0, 10000)
         value = random.randint(0, 10)
         print(f"I am generating  work with value {value}!")
-        r.hset("jobs_waiting", id, value)
+        database.hset("jobs_waiting", client_id, value)
 
 
-def emulateJobCreation(r):
-    for i in range(5):
-        generateJobs(r)
+def emulate_job_creation(database):
+    for _ in range(5):
+        generate_jobs(database)
         time.sleep(30)
 
 
-emulateJobCreation(r)
+emulate_job_creation(redis_database)
